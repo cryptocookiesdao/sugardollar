e1857ad168d7a3cedc4f715fd1181d5f83a4dd22
cryptocookiesdao <info@cryptocookiesdao.com>
2022-09-28 13:12:08 -0300

    test multi oracle

--- test/MultiOracle.t.sol
+++ test/MultiOracle.t.sol
@@ -34,10 +34,59 @@ contract BasicTokenTest is Test {
             address(feedDAI), address(feedMATIC),
             address(CKIE), address(SUSD)
         );
+
+        // SUSD - DAI twap price
+        oracleSUSD.mockprice(0.99 ether);
+        // CKIE - MATIC twap price
+        oracleCKIE.mockprice(0.7 ether);
     }
 
-    function testOracle() public {
-        // TODO
-        assertTrue(true);
+    function testMultiOracle() public {
+        vm.expectRevert("Price <= 0");
+        multiOracle.info();
+        // real world data
+        feedDAI.mockData(36893488147422279069, 99910199, 1664376182, 1664376182, 36893488147422279069);
+        feedMATIC.mockData(36893488147422307128, 74462206, 1664376356, 1664376356, 36893488147422307128);
+
+        // SUSD - DAI twap price
+        oracleSUSD.mockprice(0.89 ether);
+        // CKIE - MATIC twap price
+        oracleCKIE.mockprice(0.7 ether);
+
+        assertEq(oracleSUSD.consult(SUSD, 1 ether), 0.89 ether);
+        assertEq(multiOracle.daiPrice(), 99910199);
+        // susd in usd = 0.88920077 = usdPrice
+        uint256 usdPRICE = 0.89 ether * uint256(99910199) / 1 ether;
+        assertEq(multiOracle.susdPrice(), usdPRICE);
+
+        usdPRICE = 0.7 ether * uint256(74462206) / 1 ether;
+        assertEq(multiOracle.cookiePrice(), usdPRICE);
+    }
+
+    function testChainlinkOracle() public {
+        vm.expectRevert("Price <= 0");
+        multiOracle.daiPrice();
+
+        feedDAI.mockData(0, 1e8, 0, 0, 0);
+        vm.expectRevert("Round not complete");
+        multiOracle.daiPrice();
+
+        feedDAI.mockData(3, 1e8, 1, 1, 2);
+        vm.expectRevert("Stale price");
+        multiOracle.daiPrice();
+
+        // real world data
+        feedDAI.mockData(36893488147422279069, 99910199, 1664376182, 1664376182, 36893488147422279069);
+        assertEq(multiOracle.daiPrice(), 99910199);
+    }
+
+    function testMockOracle(uint48 _p) public {
+        // price should always be the same for this case
+        uint256 p = uint256(_p);
+
+        oracleSUSD.mockprice(p);
+        assertEq(oracleSUSD.consult(SUSD, 0.5 ether), p);
+        assertEq(oracleSUSD.consult(SUSD, 1 ether), p);
+        assertEq(oracleSUSD.consult(SUSD, 2 ether), p);
     }
 }
