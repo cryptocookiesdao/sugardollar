382c7fe102fc2a50350fa24ffd032157ff106b9e
cryptocookiesdao <info@cryptocookiesdao.com>
2022-09-28 13:59:28 -0300

    a simple vault to keep dao assets safe from crooks

---
+++ src/BankVault.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.15;
+
+import {Ownable} from  "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract BankVault is Ownable {
+    using SafeERC20 for IERC20;
+
+    IERC20 public immutable DAI;
+
+    constructor(address _dai) {
+    	DAI = IERC20(_dai);
+    }
+
+    function totalDAI() external view returns (uint256) {
+        return DAI.balanceOf(address(this));
+    }
+
+    function transferDAI(address account, uint256 amount)
+        external
+        onlyOwner
+        returns (uint256)
+    {
+        // Transfer back Collateral Token (DAI) to account
+        DAI.safeTransfer(account, amount);
+        return amount;
+    }
+}
