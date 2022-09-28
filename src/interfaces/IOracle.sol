00e6e5eacf68dbbb8610141374af6cdb84f2f4d9
cryptocookiesdao <info@cryptocookiesdao.com>
2022-09-28 13:52:13 -0300

    granny chitchat natspec

--- src/interfaces/IOracle.sol
+++ src/interfaces/IOracle.sol
@@ -1,9 +1,13 @@
 pragma solidity ^0.8.0;
 
 interface IOracle {
-    function USDCPrice() external returns (uint256);
+    function daiPrice() external returns (uint256);
 
-    function sugarDollarPrice() external returns (uint256);
+    /// @notice Returns the price of 1 SUSD based on twap price & chainlink
+    /// @return uint256 1 ether SUSD in USD
+    function susdPrice() external returns (uint256);
 
-    function cookieUSDPrice() external returns (uint256);
+    /// @notice Returns the price of 1 CKIE based on twap price & chainlink
+    /// @return uint256 1 ether CKIE in USD
+    function cookiePrice() external returns (uint256);
 }
