509e87ba5cdf18b10659974be78de3f370e8e2a5
cryptocookiesdao <info@cryptocookiesdao.com>
2022-09-30 11:15:12 -0300

    add interface function

--- src/interfaces/IGame.sol
+++ src/interfaces/IGame.sol
@@ -2,4 +2,7 @@ pragma solidity ^0.8.0;
 
 interface IGame {
     function sugarBankMint(address _to, uint256 _amount) external;
+
+    // this func is onlyOwner
+    function setSugarBankMinter(address _sugarBankMinter) external;
 }
