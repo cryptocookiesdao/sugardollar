b9080e536166060e31e7ee27753c10f6b9826285
cryptocookiesdao <info@cryptocookiesdao.com>
2022-10-01 22:32:36 -0300

    add a testeable fork/mock with foundry of uniswap

---
+++ src/mocks/uniswap/src/interfaces/IZuniswapV2Factory.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.10;
+
+interface IZuniswapV2Factory {
+    function pairs(address, address) external pure returns (address);
+
+    function createPair(address, address) external returns (address);
+
+    function getPair(address, address) external view returns (address);
+}
