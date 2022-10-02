b3d26e876a2df8bfc2b4f4604a8d4e1ccab33641
cryptocookiesdao <info@cryptocookiesdao.com>
2022-10-01 22:33:12 -0300

    versitile mock token

---
+++ src/mocks/TokenMock.sol
@@ -0,0 +1,31 @@
+/**
+ * This is a rewards token for the game CryptoCookie
+ * You cant play at https://cryptocookiesdao.com/
+ *
+ *
+ */
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+/**
+ * Token for mock porpouses
+ */
+contract TokenMock is ERC20, ERC20Burnable {
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+
+    function burn(uint256 amount) public override {
+        _burn(msg.sender, amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public override {
+        _burn(account, amount);
+    }
+
+    function mint(address _to, uint256 _amount) external {
+        _mint(_to, _amount);
+    }
+}
